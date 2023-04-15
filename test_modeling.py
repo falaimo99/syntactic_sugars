8c2f74433f351f59d707dacdc9f6c6bee3ac4443
falaimo99 <115940615+falaimo99@users.noreply.github.com>
2023-04-15 11:15:48 +0200

    Add files via upload

---
+++ test_modeling.py
@@ -0,0 +1,50 @@
+# this is just a test but it may work
+
+class IdentifiableEntity:
+    def __init__(self, identifier):
+        self.id = identifier
+
+    def getIdentifier(self):
+        return self.id
+
+
+class Annotation(IdentifiableEntity):
+    def __init__(self, identifier, body, target, motivation):
+        self.motivation = motivation
+        self.target = target
+        self.body = body
+
+        super().__init__(identifier)
+
+
+class Image(IdentifiableEntity):
+    pass
+
+class EntityWithMetadata(IdentifiableEntity):
+    def __init__(self, identifier, label, title, creators):
+        self.label = label
+        self.title = title
+        self.creators = creators
+
+        super().__init__(identifier)
+
+
+class Canvas(EntityWithMetadata):
+    def __init__(self, identifier, label, title, creators):
+        super().__init__(identifier, label, title, creators)
+
+class Manifest(EntityWithMetadata):
+    def __init__(self, identifier, label, title, creators, items):
+        self.items = items
+
+        super().__init__(identifier, label, title, creators)
+
+class Collection(EntityWithMetadata):
+    def __init__(self, identifier, label, title, creators, items):
+        self.items = items
+
+        super().__init__(identifier, label, title, creators)
+
+entity1 = IdentifiableEntity(123)
+print(entity1.getIdentifier())
+
