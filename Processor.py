04fa216be96e6d4e2ae7b42a0c393fa4e90ef8f6
Giorgia Crosilla <giorgia.crosilla@studio.unibo.it>
2023-06-11 11:51:46 +0200

    processor

--- Processor.py
+++ Processor.py
@@ -1,17 +1,12 @@
-#The processor handles the endpoint to upload the graph
-#and the path to handle the final upload of a tabular database
-
 class Processor(object): 
     def __init__(self):
         self.DbPathOrUrl = ""
         
-    def setDbPathOrUrl(self, new_path): # it needs to return a boolean
-        self.DbPathOrUrl = new_path
+    def setDbPathOrUrl(self, new_path):
+        self.DbPathOrUrl= new_path
         if new_path:
             return True 
         else: 
             return False
-        
-        
     def getDbPathOrUrl(self):
         return self.DbPathOrUrl
--- QueryProcessor.py
+++ QueryProcessor.py
@@ -1,26 +1,20 @@
 # This is a subclass of Processor, it serves the purpose of returning a
 # dataframe containing 
 
-from Processor import Processor
+from processor import Processor
 from sparql_dataframe import get
 
 class QueryProcessor(Processor):
     def __init__(self):
         super(Processor).__init__()
-        self.DbPathOrUrl = Processor.setDbPathOrUrl
+        self.DbPathorUrl = Processor.setDbPathorUrl
     
     def getEntitybyId(self, id):
 
         def getfromGraph(id):
-            endpoint = self.DbPathOrUrl
+            endpoint = self.DbPathorUrl
 
-            query = """
-                    PREFIX sysu:<https://github.com/falaimo99/syntactic_sugars/vocabulary/>
-                    select ?e 
-                    where {
-                        ?e sysu:id <%s>
-                    }
-                    """%(str(id))
+            query = "PREFIX sysu:<https://github.com/falaimo99/syntactic_sugars/vocabulary/> select ?e where {%s sysu:id ?e}"%(id)
 
             df_sparql = get(endpoint, query, True)
             
@@ -32,4 +26,5 @@ class QueryProcessor(Processor):
             pass
 
         return getfromGraph(id)
+        return getfromTabular()
     
