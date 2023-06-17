7b96c39b474e8a0522a28fedd94f9c315f5071b5
Giorgia Crosilla <giorgia.crosilla@studio.unibo.it>
2023-06-17 15:43:43 +0200

    Update csvProcessors.py

--- csvProcessors.py
+++ csvProcessors.py
@@ -10,31 +10,33 @@ class AnnotationProcessor(Processor):
     def uploadData(self, path:str):
         
         annotations = read_csv(path, keep_default_na=False, dtype={"id":"string", "body":"string", "target":"string", "motivation":"string"})
-
-        image_ids = annotations[["body"]]
-        image_internal_id = []
-        for idx, row in image_ids.iterrows():
-            image_internal_id.append("image-" + str(idx))
-
-        image_ids.insert(0, "imageId", Series(image_internal_id, dtype="string"))
-
-        annotations_ids = annotations[["id", "target", "motivation"]]
-        annotations_ids = annotations_ids.rename(columns={"id": "annotation"})
-        annotations_internal_id = []
-        for idx, row in annotations_ids.iterrows():
-            annotations_internal_id.append("annotations-" + str(idx))
-
-        annotations_ids.insert(0, "annotationsId", Series(annotations_internal_id, dtype="string"))
-        annotations_ids = annotations_ids.join(image_ids["imageId"]) 
-
-        with connect(self.DbPathOrUrl) as con:   
-            annotations_ids.to_sql("Annotation", con, if_exists="replace", index=False)
-            image_ids.to_sql("Image", con, if_exists="replace", index=False) 
-
-        if annotations_ids.empty and image_ids.empty:
-             return False
+        if annotations.empty:
+            raise ValueError("CSV file is empty")
         else:
-             return True 
+            image_ids = annotations[["body"]]
+            image_internal_id = []
+            for idx, row in image_ids.iterrows():
+                image_internal_id.append("image-" + str(idx))
+
+            image_ids.insert(0, "imageId", Series(image_internal_id, dtype="string"))
+
+            annotations_ids = annotations[["id", "target", "motivation"]]
+            annotations_ids = annotations_ids.rename(columns={"id": "annotation"})
+            annotations_internal_id = []
+            for idx, row in annotations_ids.iterrows():
+                annotations_internal_id.append("annotations-" + str(idx))
+
+            annotations_ids.insert(0, "annotationsId", Series(annotations_internal_id, dtype="string"))
+            annotations_ids = annotations_ids.join(image_ids["imageId"]) 
+
+            with connect(self.DbPathOrUrl) as con:   
+                annotations_ids.to_sql("Annotation", con, if_exists="replace", index=False)
+                image_ids.to_sql("Image", con, if_exists="replace", index=False) 
+
+            if annotations_ids.empty and image_ids.empty:
+                return False
+            else:
+                return True 
 
 
 class MetadataProcessor(Processor):
@@ -42,37 +44,40 @@ class MetadataProcessor(Processor):
         super().__init__()
     def uploadData(self, path:str):
         path1 = read_csv(path, keep_default_na=False, dtype={"id":"string", "creator":"string", "title":"string"})
-        
-        metadata = path1[["id", "title","creator"]]
-        metadata = metadata.rename(columns={"creator": "creators"})
-        metadata_id = []
-        for idx, row in metadata.iterrows():
-            metadata_id.append("metadata-" + str(idx))
-        metadata.insert(0, "metadata_internal_id", Series(metadata_id, dtype="string")) 
-        
-        z = []
-        y = []
-        for idx, row in metadata.iterrows():
-            for column_name, cell_value in row.items():
-                    if column_name == 'creators':
-                        if ';' in cell_value:
-                            cell_value = cell_value.split("; ")
-                            num_items = len(cell_value)
-                            y.extend([row.iloc[0]] * num_items)
-                            for i in cell_value:
-                                z.append(i)
-                        else:
-                            if cell_value != "":
-                                z.append(cell_value)
-                                first_element = row.iloc[0]
-                                y.append(first_element)
-        creators = pd.DataFrame({'metadata_internal_id': y, 'creator': z})
-
-        with connect(self.DbPathOrUrl) as con:   
-            metadata.to_sql("EntityWithMetadata", con, if_exists="replace", index=False)
-            creators.to_sql("creators_table", con, if_exists="replace", index=False) 
-        
-        if metadata.empty and creators.empty:
-             return False
+        if path1.empty:
+            raise ValueError("DataFrame is empty")
         else:
-             return True 
+            metadata = path1[["id", "title","creator"]]
+            metadata = metadata.rename(columns={"creator": "creators"})
+            metadata_id = []
+            for idx, row in metadata.iterrows():
+                metadata_id.append("metadata-" + str(idx))
+            metadata.insert(0, "metadata_internal_id", Series(metadata_id, dtype="string")) 
+                
+            z = []
+            y = []
+            for idx, row in metadata.iterrows():
+                for column_name, cell_value in row.items():
+                        if column_name == 'creators':
+                            if ';' in cell_value:
+                                cell_value = cell_value.split("; ")
+                                num_items = len(cell_value)
+                                y.extend([row.iloc[0]] * num_items)
+                                for i in cell_value:
+                                    z.append(i)
+                            else:
+                                if cell_value != "":
+                                    z.append(cell_value)
+                                    first_element = row.iloc[0]
+                                    y.append(first_element)
+            creators = pd.DataFrame({'metadata_internal_id': y, 'creator': z})
+
+            with connect(self.DbPathOrUrl) as con:   
+                metadata.to_sql("EntityWithMetadata", con, if_exists="replace", index=False)
+                creators.to_sql("creators_table", con, if_exists="replace", index=False) 
+                
+            if metadata.empty and creators.empty:
+                return False
+            else:
+                return True 
+            
